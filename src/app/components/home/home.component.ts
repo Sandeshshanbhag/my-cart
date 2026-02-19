e771026cabe0c51b3bb4e6db695db106b9125c6c
Sandeshshanbhag <sandeshshanbhag540@gmail.com>
2026-02-18 23:57:30 -0500

    Revert "feat: apply login page dark Apple-style theme to home page"
    This reverts commit 8abe1acd5671e46bea451d8fdcf85d218540e561.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -34,8 +34,6 @@ import { Router } from '@angular/router';
   styleUrl: './home.component.scss',
 })
 export class HomeComponent implements OnInit, OnDestroy, AfterViewChecked {
-  private el = inject(ElementRef);
-  private revealObserver: IntersectionObserver | null = null;
   searchQuery = '';
   searchResults$!: Observable<Product[]>;
   recentSearches$!: Observable<string[]>;
@@ -138,28 +136,6 @@ export class HomeComponent implements OnInit, OnDestroy, AfterViewChecked {
         this.loadNotifications(user.email);
       }
     });
-
-    // Scroll reveal animations (Apple-style)
-    this.revealObserver = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            entry.target.classList.add('visible');
-          }
-        });
-      },
-      { threshold: 0.15, rootMargin: '0px 0px -60px 0px' }
-    );
-    this.observeRevealElements();
-
-    // Watch for dynamically added reveal elements
-    const mutationObserver = new MutationObserver(() => this.observeRevealElements());
-    mutationObserver.observe(this.el.nativeElement, { childList: true, subtree: true });
-  }
-
-  private observeRevealElements(): void {
-    const elements = this.el.nativeElement.querySelectorAll('.reveal:not(.visible)');
-    elements.forEach((el: Element) => this.revealObserver?.observe(el));
   }
 
   ngOnDestroy(): void {
@@ -169,7 +145,6 @@ export class HomeComponent implements OnInit, OnDestroy, AfterViewChecked {
     if (this.cartSubscription) {
       this.cartSubscription.unsubscribe();
     }
-    this.revealObserver?.disconnect();
   }
 
   onSearchInput(): void {
